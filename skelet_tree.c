fa8c9109ab09a4fa797cfadef96cbb3774cfe7d5
IvanOfficial <31989152+IvanOfficial@users.noreply.github.com>
2018-02-11 22:17:37 +0300

    working skelet_tree.c
    sekond commit from home

--- skelet_tree.c
+++ skelet_tree.c
@@ -27,65 +27,100 @@ tree* search_by_value(tree* Head, int val)
 }
 tree* new_element_for_tree (int element)
 {
-    printf("Hello");
     tree* Head = malloc(sizeof(tree));
     Head->value = element;
     Head->right = NULL;
     Head->left = NULL;
     return Head;
 }
-void insert_into_tree (tree* Derevo, tree* element)
+void insert_into_tree (tree* Head, tree* element)
 {
-    if ((Derevo->left == NULL) && (Derevo->value > element->value))
+    printf("in func %d\n", Head->value);
+    if (Head->value <= element->value)
     {
-        Derevo->left = element;
-        return 0;
+        if (Head->right == NULL)
+        {
+            Head->right = element;
+            Head = Head->right;
+            printf("add element %d\n", Head->value);
+        }
+        if (Head->right != NULL)
+        {
+            insert_into_tree(Head->right, element);
+        }
     }
-    if ((Derevo->right == NULL) && (Derevo->value <= element->value))
+
+
+    if (Head->value > element->value)
     {
-        Derevo->right = element;
-        return 0;
+        if (Head->left == NULL)
+        {
+            Head->left = element;
+            Head = Head->left;
+            printf("add element %d\n", Head->value);
+        }
+        if (Head->left != NULL)
+        {
+            insert_into_tree(Head->left, element);
+        }
     }
-    if ((Derevo->right != NULL) && (Derevo->value <= element->value))
-    {
-        insert_into_tree(Derevo->right, element);
-    }
-    if ((Derevo->left == NULL) && (Derevo->value > element->value))
-    {
-        insert_into_tree(Derevo->left, element);
-    }
+
+
 }
 
 
-void print_tree (tree* Derevo)
+void print_tree (tree* Position)
 {
-    print_tree(Derevo->left);
-    printf("%d\n", Derevo->value);
-    print_tree(Derevo->right);
+    if (Position->left != NULL)
+    {
+        print_tree(Position->left);
+    }
+    printf("%d\n", Position->value);
+    if (Position->right != NULL)
+    {
+        print_tree(Position->right);
+    }
 }
 
 int main()
 {
     printf("Hello world!!!!!!!!!!!!!!!\n");
-    printf("Hello");
 
     tree* Ivan5 = new_element_for_tree(5);
-    printf("Hello");
     tree* Ivan1 = new_element_for_tree(1);
     tree* Ivan3 = new_element_for_tree(3);
     tree* Ivan_5 = new_element_for_tree(-5);
     tree* Ivan8 = new_element_for_tree(8);
     tree* Ivan12 = new_element_for_tree(12);
     tree* Ivan4 = new_element_for_tree(4);
+    tree* Ivan44 = new_element_for_tree(4);
+    tree* Ivan10= new_element_for_tree(10);
+    tree* Ivan9 = new_element_for_tree(9);
+    tree* Ivan11 = new_element_for_tree(11);
+    tree* Ivan55 = new_element_for_tree(5);
 
     insert_into_tree(Ivan5, Ivan1);
+    printf("1\n");
     insert_into_tree(Ivan5, Ivan3);
-    /*
+    printf("1\n");
     insert_into_tree(Ivan5, Ivan_5);
+    printf("1\n");
     insert_into_tree(Ivan5, Ivan8);
+    printf("1\n");
     insert_into_tree(Ivan5, Ivan12);
+    printf("1\n");
     insert_into_tree(Ivan5, Ivan4);
-    */
-    //print_tree(Ivan5);
+    printf("1\n");
+    insert_into_tree(Ivan5, Ivan10);
+    insert_into_tree(Ivan5, Ivan44);
+    insert_into_tree(Ivan5, Ivan9);
+    insert_into_tree(Ivan5, Ivan11);
+    insert_into_tree(Ivan5, Ivan55);
+
+
+
+    printf("%d\n", Ivan5->left->right->right->value);
+    printf("2 Yes\n");
+    print_tree(Ivan5);
     return 0;
 }
